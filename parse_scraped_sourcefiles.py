c49cd55ea3f052779653156159be9431733c0d39
GarrettArm <garrett.w.armstrong@gmail.com>
2017-03-24 08:35:03 -0500

    sundry revisions

--- parse_scraped_sourcefiles.py
+++ parse_scraped_sourcefiles.py
@@ -5,13 +5,43 @@ import os
 import pandas as pd
 
 
-output_folders = [i for i in os.listdir('.')
-                  if os.path.isdir(os.path.abspath(i)) and '_output' in i]
+def main():
+    all_df_dict = dict()
+    xl_files, csv_files = lookup_all_xl_csv_files()
+    for file in xl_files:
+        all_df_dict = parse_and_nest_xl(all_df_dict, file)
+    for file in csv_files:
+        all_df_dict = parse_and_nest_csv(all_df_dict, file)
 
-xl_files = [os.path.join(folder, file)
-            for folder in output_folders
-            for file in os.listdir(folder)
-            if os.path.splitext(file)[1] in ('.xlsx', '.xls')]
+
+def lookup_all_xl_csv_files():
+    output_folders = [i for i in os.listdir('.')
+                      if os.path.isdir(os.path.abspath(i)) and '_output' in i]
+    xl_files = [os.path.join(folder, file)
+                for folder in output_folders
+                for file in os.listdir(folder)
+                if os.path.splitext(file)[1] in ('.xlsx', '.xls')]
+    csv_files = [os.path.join(folder, file)
+                 for folder in output_folders
+                 for file in os.listdir(folder)
+                 if os.path.splitext(file)[1] == '.csv']
+    return xl_files, csv_files
+
+
+def parse_and_nest_xl(all_df_dict, file):
+    with pd.ExcelFile(file) as xl:
+        for sheet in xl.sheet_names:
+            headers_row = find_headers_row(file, sheet)
+            if headers_row:
+                df = xl.parse(sheet, header=headers_row)
+                all_df_dict = create_nested_dict(all_df_dict, file, sheet, df)
+    return all_df_dict
+
+
+def parse_and_nest_csv(all_df_dict, file):
+    df = pd.read_csv(file)
+    all_df_dict[file] = {'default': df}
+    return all_df_dict
 
 
 def find_headers_row(file, sheet):
@@ -31,33 +61,12 @@ def find_headers_row(file, sheet):
 
 
 def create_nested_dict(dictionary, key, subkey, value):
-    if not dictionary:
-        dictionary = dict()
-    if key in dictionary:
-        if subkey in key:
-            if dictionary[key][subkey] == value:
-                pass
-            else:
-                print('we never expected {}[{}][{}] to already have a value'.format(dictionary, key, subkey))
-                dictionary[key][subkey] = value
-        else:
-            dictionary[key][subkey] == value
-    else:
+    if key not in dictionary:
         dictionary[key] = {subkey: value}
+    else:
+        dictionary[key][subkey] = value
     return dictionary
 
 
-def parse_and_nest_data(all_df_dict, file):
-    with pd.ExcelFile(file) as xl:
-        for sheet in xl.sheet_names:
-            headers_row = find_headers_row(file, sheet)
-            if headers_row:
-                df = xl.parse(sheet, header=headers_row)
-                all_df_dict = create_nested_dict(all_df_dict, file, sheet, df)
-    return all_df_dict
-
-
 if __name__ == '__main__':
-    all_df_dict = dict()
-    for file in xl_files:
-        all_df_dict = parse_and_nest_data(all_df_dict, file)
+    main()
